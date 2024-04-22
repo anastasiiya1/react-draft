cc013ec1e06ee2fbb17cf924c48b19ff765b2e14
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-04-22 22:03:50 +0300

    rendering

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import { Product } from "./Product.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <h2>Best selling</h2>
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+    </div>
+  );
+}
