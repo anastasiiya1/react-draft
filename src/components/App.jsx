5e0741e6b125f0aba33db5a02154554192ffe084
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-05-07 17:19:04 +0300

    css-modules

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Product } from "./Product.jsx";
 import { BookList, Card } from "./Test.jsx";
 import { Default } from './Default.jsx'
+import { HiUser } from 'react-icons/hi'
 
 const favouriteBooks = [
 	{ id: 'id-1', name: 'JS for begginers'},
@@ -8,17 +9,26 @@ const favouriteBooks = [
 	{ id: 'id-3', name: 'React Router overview'}
 ];
 
+const UserMenu = ({ name }) => {
+  return(
+    <div>
+      <p><HiUser className="my-icon" size="24"/>{name}</p>
+    </div>
+  )
+}
+
 export default function App() {
   return (
     <div>
+    <UserMenu name="Alice" />
     <h1>Books of the week</h1>
     <BookList books={favouriteBooks}/>
     <Card>
       <h3>Card title</h3>
-      <Default variant="info">Would you like to browse our recommended products?</Default>
-      <Default variant="error">There was an error during your last transaction</Default>
-      <Default variant="success"> Payment received, thank you for your purchase</Default>
-      <Default variant="warning"> Please update your profile contact information</Default>
+      <Default variant="info" outlined elevated>Would you like to browse our recommended products?</Default>
+      <Default variant="error"outlined elevated>There was an error during your last transaction</Default>
+      <Default variant="success" elevated outlined> Payment received, thank you for your purchase</Default>
+      <Default variant="warning" elevated outlined> Please update your profile contact information</Default>
       </Card>
     
       <h2>Best selling</h2>
--- src/components/Default.jsx
+++ src/components/Default.jsx
@@ -1,32 +1,29 @@
-import './Default.css';
+import clsx from "clsx";
+import css from "./Default.module.css"
 
-  const getColor = variant => {
-	switch (variant) {
-		case 'info':
-			return "blue";
-		case 'success':
-			return "green";
-		case 'error':
-			return "red";
-		case "warning":
-			return "yellow";
-		default:
-			throw new Error(`Unsupported variant prop value - ${variant}`)				
-	}
-  }
+//   const getColor = variant => {
+// 	switch (variant) {
+// 		case 'info':
+// 			return "blue";
+// 		case 'success':
+// 			return "green";
+// 		case 'error':
+// 			return "red";
+// 		case "warning":
+// 			return "yellow";
+// 		default:
+// 			throw new Error(`Unsupported variant prop value - ${variant}`)				
+// 	}
+//   }
   
 export const Default = ({ variant, outlined, elevated, children }) => {
-	const className = ['alert', variant];
-	
-	if(outlined) {
-		className.push('is-outlined');
-	}
-
-	if (elevated) {
-		className.push('is-elevated');
-	}
-
-	return <p className={className.join(' ')} style={{
-        backgroundColor: getColor(variant),
-      }}>{children}</p>
+	return(
+		<p 
+		className ={ clsx(css[variant], {
+			[css.isOutlined]: outlined,
+			[css.isElevated]: elevated,
+		})}>
+		{children}
+	</p>
+	)
 }
