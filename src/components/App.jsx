62bc59e1be68bd32bb431a8dadac71d20a4ee940
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-05-09 16:55:07 +0300

    button events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Product } from "./Product.jsx";
 import { BookList, Card } from "./Test.jsx";
 import { Default } from './Default.jsx'
@@ -17,11 +18,73 @@ const UserMenu = ({ name }) => {
   )
 }
 
+const CustomBtn = ({ message, children}) => {
+  return (
+    <button onClick={() => alert(message)}>{children}</button>
+  )
+}
+
+// const ClickCounter = ({ value, onUpdate}) => {
+//   return <button onClick={onUpdate}>Current: {value}!</button>
+// }
+
 export default function App() {
+ const ClickCounter = () => { // let clicks = 0; 
+  const [ clicks, setClicks ] = useState(0);
+
+  const handleClick = () =>{
+     // clicks = clicks + 1;
+    setClicks(clicks + 1);
+  };
+  return <button onClick={handleClick}>Current: {clicks}!</button> 
+}
+
+// const [ clicks, setClicks ] = useState(0);
+// const handleClick = () =>{
+//   //      // clicks = clicks + 1;
+//       setClicks(clicks + 1);
+//     };
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const [values, setValues] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const updateX = () => {
+    setValues({
+      ...values,
+      x: values.x + 1
+    })
+  };
+  const updateY = () => {
+    setValues({
+      ...values,
+      y: values.y +1
+    })
+  };
   return (
     <div>
+      <p>
+        x: {values.x}, y: {values.y}
+      </p>
+      <button onClick={updateX}>Update x</button>
+      <button onClick={updateY}>Update y</button>
     <UserMenu name="Alice" />
+    <button onClick={handleToggle}>{isOpen ? "hide" : "show"}</button>
+    {isOpen && <p>Hello Alice!</p>}
     <h1>Books of the week</h1>
+    <ClickCounter></ClickCounter>
+    <ClickCounter></ClickCounter>
+    <ClickCounter ></ClickCounter>
+    {/* <button onClick={handleClick}>Current: {clicks}!</button> */}
+    <CustomBtn message="Playing music!">Play music</CustomBtn>
+    <CustomBtn message="Uploading data">Upload data</CustomBtn>
     <BookList books={favouriteBooks}/>
     <Card>
       <h3>Card title</h3>
